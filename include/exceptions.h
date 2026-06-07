432da8bd43c6d97427346828c812297c8e6d65c8
Donaldson Tan <geodome@gmail.com>
2026-06-08 03:41:31 +0800

    reformatted to make parameters easier to read

--- include/exceptions.h
+++ include/exceptions.h
@@ -4,13 +4,15 @@
 #include <exception>
 #include <string>
 
-class myexception: public std::except {
+class myexception: public std::exception {
     std::string message;
 public:
     myexception(char* msg): message{std::string{msg}} {};
     myexception(std::string msg): message{std::move(msg)} {};
-    myexception(std::string tag, std::string msg): message{std::format("{} : {}", tag, msg)} {}
-    myexception(std::string tag, char* msg): message{std::format("{} : {}", tag, std::string{msg})} {}
+    myexception(std::string tag, std::string msg):
+        message{std::format("{} : {}", tag, msg)} {}
+    myexception(std::string tag, char* msg):
+        message{std::format("{} : {}", tag, std::string{msg})} {}
     std::string what() {
         return message;
     }
@@ -26,14 +28,15 @@ public:
 
 class SDL_Cannot_Load_Media: public myexception {
     SDL_Cannot_Load_Media(char* file, std::string msg):
-    myexception("SDL Cannot Load Media",
-                std::format("{} : {}",
-                std::string{file}, msg)) {};
-    SDL_Cannot_Cannot_Load_Media(char* file, char* msg):
-    myexception("SDL Cannot Load Media",
-                std::format("{} : {}",
-                std::string:file},
-                std::string{msg})) {};
+    myexception(
+                "SDL Cannot Load Media",
+                std::format("{} : {}", std::string{file}, msg)
+    ) {};
+    SDL_Cannot_Load_Media(char* file, char* msg):
+    myexception(
+                "SDL Cannot Load Media",
+                std::format("{} : {}", std::string{file},std::string{msg})
+    ) {};
 };
 
 #endif
