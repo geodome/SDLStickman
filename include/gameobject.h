2a545a410c17ef876323390fcc5dac894ce955e4
Donaldson Tan <geodome@gmail.com>
2026-06-08 06:20:25 +0800

    defined member functions for Stickman class

--- include/gameobject.h
+++ include/gameobject.h
@@ -1,14 +1,15 @@
 #ifndef GAMEOBJECT_H
 #define GAMEOBJECT_H
 
+#include <string>
 #include <SDL2/SDL.h>
 #include "exceptions.h"
 
 class GameObject {
 public:
-    virtual void draw(SDL_Surface*);
-    virtual void render(SDL_Window*);
-    virtual ~GameObject() {};
+    GameObject() = default;
+    virtual void draw(SDL_Surface*) {};
+    virtual ~GameObject() = default;
 };
 
 class Stickman: public GameObject {
@@ -20,9 +21,41 @@ public:
     Stickman();
     ~Stickman();
     void reset_frame();
-    void next_frame(SDL_Surface* gSurface);
+    void next_frame();
     void draw(SDL_Surface* gSurface) override;
-    void render(SDL_Window* gSurface) override;
 };
 
+Stickman::Stickman():GameObject() {
+    std::string file {"/Users/donaldsontan/projects/SDLStickman/SDLStickman/assets/stickman.bmp"};
+    bmp = SDL_LoadBMP(file.c_str());
+    if(bmp == nullptr) {
+        auto msg = std::string{SDL_GetError()};
+        throw SDL_Cannot_Load_Media(file, msg);
+    }
+}
+
+Stickman::~Stickman() {
+    SDL_FreeSurface(bmp);
+}
+
+void Stickman::reset_frame() {
+    i = -1;
+    source = {0, 0, WIDTH, HEIGHT};
+}
+
+void Stickman::next_frame() {
+    i = (i + 1) % 8;
+    int x = i % 4;
+    int y = i / 4;
+    source.x = x*WIDTH;
+    source.y = y*HEIGHT;
+}
+
+void Stickman::draw(SDL_Surface* gSurface) {
+    next_frame();
+    SDL_BlitSurface(bmp, &source, gSurface, &destination);
+}
+
+
 #endif
+
