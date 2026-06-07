161b29b752acb8b95f0801408f83237000814d82
Donaldson Tan <geodome@gmail.com>
2026-06-08 01:48:12 +0800

    defined exceptions SDL_Cannot_Init and SDL_Cannot_Load_Media

---
+++ include/exceptions.h
@@ -0,0 +1,34 @@
+#include <exception>
+#include <string>
+
+class myexception: public std::except {
+    std::string message;
+public:
+    myexception(char* msg): message{std::string{msg}} {};
+    myexception(std::string msg): message{std::move(msg)} {};
+    myexception(std::string tag, std::string msg): message{std::format("{} : {}", tag, msg)} {}
+    myexception(std::string tag, char* msg): message{std::format("{} : {}", tag, std::string{msg})} {}
+    std::string what() {
+        return message;
+    }
+}
+
+class SDL_Cannot_Init: public myexception {
+public:
+    SDL_Cannot_Init(std::string msg):
+    myexception("SDL Cannot Init", msg) {};
+    SDL_Cannot_Init(char* msg):
+    myexception("SDL Cannot Init", msg) {};
+}
+
+class SDL_Cannot_Load_Media: public myexception {
+    SDL_Cannot_Load_Media(char* file, std::string msg):
+    myexception("SDL Cannot Load Media",
+                std::format("{} : {}",
+                std::string{file}, msg)) {};
+    SDL_Cannot_Cannot_Load_Media(char* file, char* msg):
+    myexception("SDL Cannot Load Media",
+                std::format("{} : {}",
+                std::string:file},
+                std::string{msg})) {};
+}
