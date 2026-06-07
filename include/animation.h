45212ccdc78c3e59b410f1c1f7ad8bddc3d7f30e
Donaldson Tan <geodome@gmail.com>
2026-06-08 01:59:41 +0800

    added register_handler

--- include/animation.h
+++ include/animation.h
@@ -6,7 +6,9 @@
 //struct SDL_Event {};
 
 class Animation {
-    const int DELAY{100}, WIDTH{}, HEIGHT{};
+    const int DELAY{100}, WIDTH{403}, HEIGHT{363};
+    SDL_Window* gWindow;
+    SDL_Surface* gSurface;
     std::map<uint32_t, std::vector<std::function<void(SDL_Event,bool&,bool&)>>> handlers{};
 public:
     Animation();
@@ -24,30 +26,4 @@ void Animation::register_handler(uint32_t event_type, std::function<void(SDL_Eve
     handlers[event_type].push_back(f);
 }
 
-void Animation::handle_events(bool& quit, bool& suspended) {
-    SDL_Event e;
-    while(SDL_PollEvent(&e)) {
-        for(auto f: handlers[e.type]){
-            f(e, quit, suspended);
-            if(quit) break;
-        }
-        if(quit) break;
-    }
-}
-
-void Animation::main_loop() {
-    bool quit{false}, suspended{false};
-    register_handler(SDL_QUIT, [] (SDL_Event e, bool& quit, bool& suspended) {
-        quit = true;
-        suspended = true;
-    });
-    while(!quit) {
-        handle_events(quit, suspended);
-        if(!suspended) {
-            update();
-            render();
-        }
-        SDL_Delay(DELAY);
-    }
-}
 #endif
