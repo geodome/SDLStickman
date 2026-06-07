a878172323bccd405f5c413349ca71299600e6df
Donaldson Tan <geodome@gmail.com>
2026-06-08 07:43:23 +0800

    added handlers

--- include/gameobject.h
+++ include/gameobject.h
@@ -1,19 +1,33 @@
 #ifndef GAMEOBJECT_H
 #define GAMEOBJECT_H
 
+#include <map>
+#include <vector>
+#include <functional>
 #include <string>
 #include <SDL2/SDL.h>
 #include "exceptions.h"
 
 class GameObject {
+    std::map<uint32_t, std::vector<std::function<void(SDL_Event,bool&, bool&)>>> handlers{};
 public:
     GameObject() = default;
-    virtual void draw(SDL_Surface*) {};
     virtual ~GameObject() = default;
+    virtual void draw(SDL_Surface*) {};
+    void add_handler(uint32_t, std::function<void(SDL_Event,bool&, bool&)>);
+    std::map<uint32_t, std::vector<std::function<void(SDL_Event,bool&, bool&)>>> get_handlers();
 };
 
+std::map<uint32_t, std::vector<std::function<void(SDL_Event,bool&, bool&)>>> GameObject::get_handlers() {
+    return handlers;
+}
+
+void GameObject::add_handler(uint32_t event_type, std::function<void(SDL_Event,bool&, bool&)> f) {
+    handlers[event_type].push_back(f);
+}
+
 class Stickman: public GameObject {
-    const int WIDTH{403}, HEIGHT{363};
+    const int WIDTH{400}, HEIGHT{366};
     int i{-1};
     SDL_Surface* bmp;
     SDL_Rect source{0,0,WIDTH,HEIGHT}, destination{0,0,WIDTH,HEIGHT};
@@ -32,6 +46,10 @@ Stickman::Stickman():GameObject() {
         auto msg = std::string{SDL_GetError()};
         throw SDL_Cannot_Load_Media(file, msg);
     }
+    // toggles the animation on/off with a mouse click
+    add_handler(SDL_MOUSEBUTTONDOWN, [] (SDL_Event e, bool& quit, bool& suspended) {
+        suspended = !suspended;
+    });
 }
 
 Stickman::~Stickman() {
@@ -56,6 +74,6 @@ void Stickman::draw(SDL_Surface* gSurface) {
     SDL_BlitSurface(bmp, &source, gSurface, &destination);
 }
 
-
 #endif
+
 
