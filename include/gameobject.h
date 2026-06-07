29154c61d214bdd842fbb36ea8681bc1bf896d22
Donaldson Tan <geodome@gmail.com>
2026-06-08 05:12:41 +0800

    declared member functions of Stickman

--- include/gameobject.h
+++ include/gameobject.h
@@ -2,14 +2,27 @@
 #define GAMEOBJECT_H
 
 #include <SDL2/SDL.h>
-#include "exeptions.h"
+#include "exceptions.h"
 
 class GameObject {
 public:
     virtual void draw(SDL_Surface*);
     virtual void render(SDL_Window*);
+    virtual ~GameObject() {};
 };
 
-
+class Stickman: public GameObject {
+    const int WIDTH{403}, HEIGHT{363};
+    int i{-1};
+    SDL_Surface* bmp;
+    SDL_Rect source{0,0,WIDTH,HEIGHT}, destination{0,0,WIDTH,HEIGHT};
+public:
+    Stickman();
+    ~Stickman();
+    void reset_frame();
+    void next_frame(SDL_Surface* gSurface);
+    void draw(SDL_Surface* gSurface) override;
+    void render(SDL_Window* gSurface) override;
+};
 
 #endif
