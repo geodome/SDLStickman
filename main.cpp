a5a04f8abc8644de981170ce397edab32be23336
Donaldson Tan <geodome@gmail.com>
2026-06-09 08:22:14 +0800

    replaced cout with cerr for error msg

--- main.cpp
+++ main.cpp
@@ -7,10 +7,10 @@ int main(int argc, char* argv[]) {
         Animation anime{};
         anime.main_loop();
     } catch(SDL_Cannot_Init& e) {
-        std::cout << e.what() << std::endl;
+        std::cerr << e.what() << std::endl;
         return 1;
     } catch(SDL_Cannot_Load_Media& e) {
-        std::cout << e.what() << std::endl;
+        std::cerr << e.what() << std::endl;
         return 1;
     }
     std::cout << "program exiting" << std::endl;
