e8b38025ccee5b7dba38dcef45454acc8d14aebf
Donaldson Tan <geodome@gmail.com>
2026-06-12 00:42:41 +0800

    added vector2d

---
+++ src/vector2d..cpp
@@ -0,0 +1,53 @@
+#include "vector2d.h"
+#include <iostream>
+#include <stdexcept>
+
+Vector2D Vector2D::operator+(Vector2D other) {
+    return {x + other.x, y + other.y};
+}
+
+Vector2D& Vector2D::operator+=(Vector2D other) {
+    x += other.x;
+    y += other.y;
+    return *this;
+}
+
+Vector2D Vector2D::operator-(Vector2D other) {
+    return {x - other.x, y - other.y};
+}
+
+Vector2D& Vector2D::operator-=(Vector2D other) {
+    x -= other.x;
+    y -= other.y;
+    return *this;
+}
+
+Vector2D Vector2D::operator*(double a) {
+    return {x*a, y*a};
+}
+
+Vector2D& Vector2D::operator*=(double a) {
+    x *= a;
+    y *= a;
+    return *this;
+}
+
+Vector2D Vector2D::operator/(double a) {
+    if(a == 0) throw std::invalid_argument("division by zero error");
+    return Vector2D(x/a, y/a);
+}
+
+Vector2D& Vector2D::operator/=(double a) {
+    if(a == 0) throw std::invalid_argument("division by zero error");
+    x /= a;
+    y /= a;
+    return *this;
+}
+
+
+std::ostream& operator<<(std::ostream& out, const Vector2D& other) {
+    out << "(" << other.x << "," << other.y << ")";
+    return out;
+}
+
+
