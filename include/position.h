d9299e6c54be2fea3ff1ce79d778da2e6526e6af
Donaldson Tan <geodome@gmail.com>
2026-06-12 00:55:51 +0800

    added position

---
+++ include/position.h
@@ -0,0 +1,23 @@
+#ifndef POSITION_H
+#define POSITION_H
+
+#include "vector2d.h"
+
+class Position {
+    Vector2D coord{0,0}, velocity{0,0}, acceleration{0,0};
+public:
+    Position(double x, double y): coord{x,y} {}
+    ~Position() = default;
+    Vector2D get_coord();
+    Vector2D get_velocity();
+    Vector2D get_acceleration();
+    void update();
+    Vector2D next_coord();
+    void stop();
+    void set_coord(double, double);
+    void set_velocity(double, double);
+    void set_acceleration(double, double);
+};
+
+#endif
+
---
+++ src/position.cpp
@@ -0,0 +1,44 @@
+#include "vector2d.h"
+#include "position.h"
+
+Vector2D Position::get_coord() {
+    return coord;
+}
+
+Vector2D Position::get_velocity() {
+    return velocity;
+}
+
+Vector2D Position::get_acceleration() {
+    return acceleration;
+}
+
+void Position::update() {
+    coord += velocity;
+    velocity += acceleration;
+}
+
+Vector2D Position::next_coord() {
+    return coord + velocity;
+}
+
+void Position::set_coord(double x, double y) {
+    coord.x = x;
+    coord.y = y;
+}
+
+void Position::set_velocity(double x, double y) {
+    velocity.x = x;
+    velocity.y = y;
+}
+
+void Position::set_acceleration(double x, double y) {
+    acceleration.x = x;
+    acceleration.y = y;
+}
+
+void Position::stop() {
+    velocity = {0,0};
+    acceleration = {0,0};
+}
+
