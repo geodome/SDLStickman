e48e80d675ad9b116ef61ce05462122192d62b0e
Donaldson Tan <geodome@gmail.com>
2026-06-12 01:26:53 +0800

    Added namespace system

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 
 int main(int argc, char* argv[]) {
     try {
-        System anime{};
+        System::System anime{};
         anime.main_loop();
     } catch(SDL_Cannot_Init& e) {
         std::cerr << e.what() << std::endl;
