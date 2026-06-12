39ce9fb4a3c4aa200be6d233153c57257ae31410
Donaldson Tan <geodome@gmail.com>
2026-06-13 04:25:03 +0800

    added namespaces

--- main.cpp
+++ main.cpp
@@ -1,19 +1,21 @@
-#include "system.h"
-#include "exceptions.h"
 #include <iostream>
+#include "exceptions.h"
+#include "system.h"
 
 int main(int argc, char* argv[]) {
+    
     try {
-        System::System anime{};
-        anime.main_loop();
-    } catch(SDL_Cannot_Init& e) {
+        System::Singleton()->main_loop();
+        System::Destroy_Singleton();
+    } catch(System::SDL_Cannot_Init& e) {
         std::cerr << e.what() << std::endl;
         return 1;
-    } catch(SDL_Cannot_Load_Media& e) {
+    } catch(GameObjects::SDL_Cannot_Load_Media& e) {
         std::cerr << e.what() << std::endl;
+        SDL_Quit();
         return 1;
     }
-    std::cout << "program exiting" << std::endl;
     return 0;
+    
 }
 
